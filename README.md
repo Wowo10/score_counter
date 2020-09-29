79b66702063157781ffc042a04ff4544cbdaec37
Wowo10 <wojtekwojtek529@gmail.com>
2020-09-29 23:38:20 +0200

    Initial commit - an MVP.

---
+++ README.md
@@ -0,0 +1,7 @@
+# score_counter
+
+My Flutter tryout.
+
+## Getting Started
+
+Simple score counter app for board games like Love Letter.
---
+++ lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Score Counter',
+      theme: ThemeData(
+        primaryColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ScoreCounter(title: 'Score Counter'),
+    );
+  }
+}
+
+class PlayerDto {
+  PlayerDto({this.name, this.score});
+
+  String name;
+  int score;
+}
+
+class ScoreCounter extends StatefulWidget {
+  ScoreCounter({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _ScoreCounterState createState() => _ScoreCounterState();
+}
+
+class _ScoreCounterState extends State<ScoreCounter> {
+  final _playersList = <PlayerDto>[];
+  final _biggerFont = TextStyle(fontSize: 22.0);
+
+  _ScoreCounterState() {
+    _playersList.add(PlayerDto(name: 'Player1', score: 0));
+    _playersList.add(PlayerDto(name: 'Player2', score: 0));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final rows = _playersList.map((e) => _buildRow(e));
+    final dividedTiles = ListTile.divideTiles(context: context, tiles: rows);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: dividedTiles.toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _playersList.add(PlayerDto(
+                name: 'Player' + _playersList.length.toString(), score: 0));
+          });
+        },
+        tooltip: 'Add Player',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Widget _buildRow(PlayerDto dto) {
+    return ListTile(
+      title: Text(
+        dto.name + ': ' + dto.score.toString(),
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        Icons.plus_one,
+        color: Colors.black,
+      ),
+      onTap: () {
+        setState(() {
+          dto.score += 1;
+        });
+      },
+      onLongPress: () {
+        setState(() {
+          dto.score -= 1;
+        });
+      },
+    );
+  }
+}
